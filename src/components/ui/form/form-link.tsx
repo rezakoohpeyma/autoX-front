7cf63123f47eed9c25d99e4a33c3574bca0072a3
Milad Afzali <miladsteam455@gmail.com>
2026-06-16 01:35:00 +0330

    fix(form-link): remove mt-10 style

--- src/components/ui/form/form-link.tsx
+++ src/components/ui/form/form-link.tsx
@@ -1,15 +1,17 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { ComponentProps } from "react";
 
 export function FormLink({
   question,
   href = "",
+  className,
   children,
 }: ComponentProps<"link"> & {
   question?: string;
 }) {
   return (
-    <p className="text-gray-400 mt-10 text-sm">
+    <p className="text-gray-400 text-sm">
       {question && (
         <>
           <span>{question}</span> &nbsp;
@@ -17,7 +19,7 @@ export function FormLink({
       )}
       <Link
         href={href}
-        className="text-blue-600 font-semibold mt-4 transition hover:border-b hover:border-blue-600"
+        className={cn("text-blue-600 font-semibold mt-4 transition hover:border-b hover:border-blue-600", className || '')}
       >
         {children}
       </Link>
