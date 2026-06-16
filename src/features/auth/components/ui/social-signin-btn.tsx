9c169ca409d43aa3e869b1509c4160b81d4c4316
Milad Afzali <miladsteam455@gmail.com>
2026-06-17 00:41:43 +0330

    style(sign-in): add hover to social-signin-btn

--- src/features/auth/components/ui/social-signin-btn.tsx
+++ src/features/auth/components/ui/social-signin-btn.tsx
@@ -3,10 +3,10 @@ import { JSX } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 export default function SocialSigninBtn({ href , company } : { href?: string, company: 'google' | 'github' }): JSX.Element {
     return (
-        <Link href={href || ''} className='bg-secondary w-97 h-13 rounded-xl px-2.25 py-3 flex justify-center items-center gap-4.5'> 
+        <Link href={href || ''} className='bg-secondary font-normal transition-all delay-75 duration-300 w-97 h-13 rounded-xl px-2.25 py-3 flex justify-center items-center gap-4.5 hover:font-bold'> 
             {company === 'github' && <FaGithub size={28} className='text-primary'/>}
             {company === 'google' && <FaGoogle size={28} className='text-primary'/>}
-            <span className='font-normal text-base text-primary/80'>
+            <span className='text-base text-primary/80'>
                 {company === 'github' && 'Sign in with Github'}
                 {company === 'google' && 'Sign in with Google'}
             </span>
