a64b1a222c01cfb7fa05c5ad3479f43fa7f288c6
Milad Afzali <miladsteam455@gmail.com>
2026-06-17 18:48:26 +0330

    feat(sign-up): add sign-up schema

--- src/features/auth/schema.ts
+++ src/features/auth/schema.ts
@@ -1,9 +1,21 @@
 import { z } from 'zod'
-export  const signInSchema = z.object({
+export const signInSchema = z.object({
     username: z.string().min(2, 'Username must be more than 2 characters.'),
     password: z.string().min(8, "Password must be more than 8 characters.")
 })
 
+export const signUpSchema = z.object({
+    firstName: z.string().min(2, 'First Name must be more than 2 characters.'),
+    lastName: z.string().min(2, 'Last Name must be more than 2 characters.'),
+    username: z.string().min(2, 'Username must be more than 2 characters.'),
+    password: z.string().min(8, "Password must be more than 8 characters."),
+    confirmPassword: z.string().min(8, "Password must be more than 8 characters."),
+    rules: z.boolean(),
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword']
+})
 
 
 export type SignInType = z.infer<typeof signInSchema>
+export type SignUpType = z.infer<typeof signUpSchema>
