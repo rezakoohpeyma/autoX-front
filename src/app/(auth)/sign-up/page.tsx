e66e3a9304ad3e1e359325959a2170ffa8a8eea2
Milad Afzali <miladsteam455@gmail.com>
2026-06-17 18:47:41 +0330

    feat(sign-up-form): create sign-up-form.tsx & use in sign-up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,9 @@
+import SignUpForm from '@/features/auth/components/sign-up-form';
 import { JSX } from 'react';
 export default function SignUpPage(): JSX.Element {
     return (
         <div>
-            
+            <SignUpForm />
         </div>
     )
 }
---
+++ src/features/auth/components/sign-up-form.tsx
@@ -0,0 +1,75 @@
+'use client';
+import { JSX } from 'react';
+import AuthForm from './ui/auth-form';
+import { SubmitHandler } from 'react-hook-form';
+import { signUpSchema, SignUpType } from '../schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormWraper } from '@/components/ui/form/form-wraper';
+import { FormTitle } from '@/components/ui/form/form-tiitle';
+import { Input } from '@/components/ui/form/input';
+import { Checkbox } from '@/components/ui/form/checkbox';
+import { FormLink } from '@/components/ui/form/form-link';
+import SubmitBtn from './ui/submit-btn';
+import SocialSignup from './social-signup';
+export default function SignUpForm(): JSX.Element {
+    const onSubmit: SubmitHandler<SignUpType> = () => {}
+    const formOpt = {
+        resolver: zodResolver(signUpSchema),
+    }
+    return (
+        <AuthForm 
+            onSubmit={onSubmit} 
+            formOpt={formOpt}
+            className='w-113.25 p-6 flex justify-center items-center flex-col gap-7.25 mx-auto'
+        >
+            <FormWraper className='text-center'>
+                <FormTitle className='mb-2 text-3xl font-bold text-primary'>Sign Up</FormTitle>
+                <p className='text-primary/80 font-normal text-sm'>Create your account to get started</p>
+            </FormWraper>
+            <FormWraper>
+                <FormWraper className='flex justify-center items-start gap-4'>
+                    <Input 
+                        nameId='firstName'
+                        placeholder='Milad'
+                    >
+                        First Name
+                    </Input>
+                    <Input 
+                        nameId='lastName'
+                        placeholder='Afzali'
+                    >
+                        Last Name
+                    </Input>
+                </FormWraper>
+                <Input 
+                    nameId='username'
+                    placeholder='MiladAfzali123'
+                >
+                    Username
+                </Input>
+                <Input 
+                    nameId='password'
+                    type='password'
+                    placeholder='At least 8 characters'
+                >
+                    Password
+                </Input>
+                <Input 
+                    type='password'
+                    nameId='confirmPassword'
+                    placeholder='At least 8 characters'
+                >
+                    Confirm Password
+                </Input>
+                <Checkbox nameId='rules' checkboxContainerClasses='my-6'>
+                    <FormLink href='#' question='I agree to the' linkClasses='text-primary hover:border-primary'>Terms and Conditions</FormLink>
+                </Checkbox>
+                <SubmitBtn>Create Account</SubmitBtn>
+            </FormWraper>
+            <FormWraper>
+                <FormLink href='/sign-in' question='Already have an account?' className='text-center text-sm font-normal' linkClasses='text-link hover:border-link'>Sign in</FormLink>
+            </FormWraper>
+        </AuthForm>
+    )
+}
+
