2c815f9ef3ef247f874800666d4710f66407160d
Milad Afzali <miladsteam455@gmail.com>
2026-06-17 14:48:03 +0330

    refactor(auth): move submit-btn.tsx to ui components & add children props

--- src/features/auth/components/ui/submit-btn.tsx
+++ src/features/auth/components/ui/submit-btn.tsx
@@ -1,9 +1,9 @@
 import { FormSubmitBtn } from '@/components/ui/form/form-submit-button';
 import { JSX } from 'react';
-export default function SubmitBtn(): JSX.Element {
+export default function SubmitBtn({ children } : { children: React.ReactNode }): JSX.Element {
     return (
         <FormSubmitBtn className='bg-primary transition-all delay-75 duration-300 text-white h-13 rounded-xl font-normal text-[20px] hover:bg-primary/80'>
-            Sign in
+            {children}
         </FormSubmitBtn>
     )
 }
