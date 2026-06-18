24f80da66230adaefca9e82d581854c80e65c270
Milad Afzali <miladsteam455@gmail.com>
2026-06-18 12:40:17 +0330

    feat(auth): create reusable divider.tsx & use sign-in and sign-up form

--- src/features/auth/components/ui/divider.tsx
+++ src/features/auth/components/ui/divider.tsx
@@ -1,7 +1,9 @@
+import { cn } from '@/lib/utils';
 import { JSX } from 'react';
-export default function Divider({ children } : { children: React.ReactNode }): JSX.Element {
+export default function Divider({ children, className }
+     : { children: React.ReactNode, className?: string }): JSX.Element {
     return (
-        <div className='h-0.5 w-full bg-primary/30 relative flex justify-center items-center'>
+        <div className={cn('h-0.5 w-full bg-primary/30 relative flex justify-center items-center', className || '')}>
             <span className='block bg-white px-4 text-primary/80 text-center'>{children}</span>
         </div>
     )
