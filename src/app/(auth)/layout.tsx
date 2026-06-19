c95e370954849036bc04222bd5dec3e3a05d3f84
Milad Afzali <miladsteam455@gmail.com>
2026-06-19 15:20:56 +0330

    style(auth): adjust vertical padding in AuthLayout for mobile

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }):
     return (
         <div className='p-8 h-full w-full'>
             <div className='relative h-full w-full flex flex-col-reverse justify-between items-center gap-4 lg:gap-8 lg:flex-row'>
-                <div className='flex-1 w-full lg:overflow-y-scroll py-12 lg:py-20 h-screen'>
+                <div className='flex-1 w-full lg:overflow-y-scroll py-7 lg:py-20 h-screen'>
                     {children}
                 </div>
                 <AuthBanner />
