5dd86f58d9fb08bbbe37a76ad7c7f3cbb61f9db4
Milad Afzali <miladsteam455@gmail.com>
2026-06-20 23:57:48 +0330

    feat(layout): implement protected dashboard layout with sidebar
    - Add Sidebar component to ProtectedLayout.
    - Setup flexbox container for sidebar and main content area.
    - Apply dashboard background color and prevent body overflow.

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,12 @@
+import Sidebar from '@/features/sidebar/components/sidebar';
 import { JSX } from 'react';
 export default function ProtectedLayout({ children } : { children: React.ReactNode }): JSX.Element {
     return (
-        <div>
-           {children} 
+        <div className='p-4.5 bg-dashboard-bg h-screen overflow-hidden flex gap-4.5'>
+            <Sidebar />
+           <div className='flex-1'>
+                {children} 
+           </div>
         </div>
     )
 }
