9cda1d93443df70e0e4cad80a885a98173ce4a22
Milad Afzali <miladsteam455@gmail.com>
2026-06-21 15:17:32 +0330

    feat: implement collapsible submenu for sidebar items
    - Adds state management to  to track the currently selected/expanded item.
    - Updates  to support:
      - Toggle functionality for submenus.
      - Dynamic chevron icons (/).
      - Rendering of  components when an item is expanded.
    - Improves  layout to accommodate parent-child hierarchy structure.

--- src/features/sidebar/components/sidebar-category.tsx
+++ src/features/sidebar/components/sidebar-category.tsx
@@ -1,4 +1,5 @@
-import { JSX } from 'react';
+'use client';
+import { JSX, useState } from 'react';
 import { SidebarCategoryType } from '../types/sidebar-types';
 import SidebarItem from './sidebar-item';
 
@@ -7,11 +8,14 @@ interface SideBarCategoryProp {
 }
 
 export default function SidebarCategory({ category } : SideBarCategoryProp): JSX.Element {
+    const [selectedId, setSelectedId] = useState<number | null>(null);
+
+    const handleSelectedId = (id : number) => setSelectedId(lastId => lastId === id ? null : id);
     return (
         <div className='px-6 pt-6'>
             <h3 className='font-medium text-[10px] tracking-wider text-primary/80 uppercase mb-2'>{category.name}</h3>
             <div className='space-y-2'>
-                {category.items.map((item, i) => <SidebarItem item={item} key={i}/>)}
+                {category.items.map((item, i) => <SidebarItem onSelectedId={handleSelectedId} selectedId={selectedId} item={item} key={i}/>)}
             </div>
             
         </div>
--- src/features/sidebar/components/sidebar-item.tsx
+++ src/features/sidebar/components/sidebar-item.tsx
@@ -2,19 +2,37 @@ import * as ReactIcons from "react-icons/fa6";
 import { JSX} from 'react';
 import { SidebarItemType } from '../types/sidebar-types';
 import NavLink from "@/components/ui/nav-link";
+import SidebarItemChild from "./sidebar-item-child";
 
 interface SidebarItemProp {
     item: SidebarItemType
+    selectedId: number | null,
+    onSelectedId: (id: number) => void,
 }
 
-export default function SidebarItem({ item }: SidebarItemProp): JSX.Element {
+export default function SidebarItem({ item, onSelectedId, selectedId }: SidebarItemProp): JSX.Element {
+    const isSelect = selectedId === item.id;
     const IconComponent = item.icon ? ReactIcons[item.icon] : ReactIcons.FaQuestion;
     return (
-        <div>
-            <NavLink href={item.link} className="flex items-center gap-2 h-10 rounded-lg p-3 cursor-pointer">
-                {<IconComponent className="text-[20px] text-[#5C5E64]"/>}
-                <span className="font-medium text-sm tracking-tight">{item.name}</span>
+        <div onClick={() => onSelectedId(item.id)}>
+            <NavLink href={item.link} className="flex justify-between items-center h-10 rounded-lg p-3 cursor-pointer">
+                <span className="flex items-center gap-2">
+                    {<IconComponent className="text-[20px] text-[#5C5E64]"/>}
+                    <span className="font-medium text-sm tracking-tight">{item.name}</span>
+                </span>
+                {item.hasChild && isSelect 
+                    ? <ReactIcons.FaAngleUp className="text-base text-[#5C5E64]" /> 
+                    : <ReactIcons.FaAngleDown className="text-base text-[#5C5E64]" />
+                }
             </NavLink>
+            {isSelect && 
+                <div className="py-3 w-3/5 ml-auto flex justify-center items-stretch gap-3">
+                    <div className="w-0.5 bg-sidebar-border" />
+                    <div className="flex justify-center items-center flex-col gap-1 w-full">
+                        {item.hasChild?.map((subItem, i) => <SidebarItemChild key={i} item={subItem}/>)}
+                    </div>
+                </div>
+            }
         </div>
     );
 }
