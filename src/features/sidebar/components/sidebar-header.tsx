c3431409a13476c332aec97e9b058cd06e420a8c
Milad Afzali <miladsteam455@gmail.com>
2026-06-21 15:19:46 +0330

    fix(sidebar-header): replace hardcode bg style to bg-white-secondary

--- src/features/sidebar/components/sidebar-header.tsx
+++ src/features/sidebar/components/sidebar-header.tsx
@@ -3,7 +3,7 @@ import { JSX } from 'react';
 import { FaEllipsisVertical } from 'react-icons/fa6';
 export default function SidebarHeader(): JSX.Element {
     return (
-        <div className='bg-[#FAFAFA] h-18 border-b-2 border-sidebar-border p-6 flex justify-between items-center'>
+        <div className='bg-white-secondary h-18 w-full border-b-2 border-sidebar-border p-6 flex justify-between items-center'>
             <Logo />
             <button className='text-primary text-xl cursor-pointer'>
                 <FaEllipsisVertical/>
