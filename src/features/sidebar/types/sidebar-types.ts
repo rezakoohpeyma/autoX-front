66f01aea3186510762df2624e62a9ff57f9d37b9
Milad Afzali <miladsteam455@gmail.com>
2026-06-21 15:12:08 +0330

    feat(sidebar): extend sidebar types with child items
    - Adds  for nested sidebar entries.
    - Extends  with optional  support.
    - Keeps type definitions consistent with the updated sidebar structure.

--- src/features/sidebar/types/sidebar-types.ts
+++ src/features/sidebar/types/sidebar-types.ts
@@ -1,14 +1,21 @@
 import * as ReactIcons from "react-icons/fa6";
 
+export type SidebarItemChildType = {
+    id: number,
+    name: string,
+    link: string,
+}
+
 export type SidebarItemType = {
     id: number,
     name: string,
     link: string,
-    icon: keyof typeof ReactIcons | null; 
+    icon: keyof typeof ReactIcons | null,
+    hasChild?: SidebarItemChildType[],
 }
 
 export type SidebarCategoryType = {
     id: number,
     name: string,
-    items: SidebarItemType[]
+    items: SidebarItemType[],
 }
