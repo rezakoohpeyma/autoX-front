e994094f10236966b990cb959cc6faa609a26cca
Milad Afzali <miladsteam455@gmail.com>
2026-06-21 15:20:57 +0330

    test(sidebar): add sidebar data for test design & sidebar logic

--- src/features/sidebar/config/sidebar.ts
+++ src/features/sidebar/config/sidebar.ts
@@ -1,6 +1,6 @@
 import { SidebarCategoryType } from "../types/sidebar-types";
 
-export const sidebar: SidebarCategoryType[] = Array.from({ length : 5 }, (i: number) => {
+export const sidebar: SidebarCategoryType[] = Array.from({ length : 10 }, (i: number) => {
     return {
         id: i,
         name: "Test" + i,
@@ -10,6 +10,23 @@ export const sidebar: SidebarCategoryType[] = Array.from({ length : 5 }, (i: num
                 name: "SubTest1",
                 link: "/",
                 icon: null,
+                hasChild: [
+                    {
+                        id: 0,
+                        name: 'test1',
+                        link: '/'
+                    },
+                    {
+                        id: 1,
+                        name: 'test2',
+                        link: 'test3'
+                    },
+                    {
+                        id: 3,
+                        name: 'test3',
+                        link: 'test2'
+                    }
+                ]
             },
             {
                 id: 1,
