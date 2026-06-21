e1b3bcdfdd54a1d574488e98f4209bba62a244ad
Milad Afzali <miladsteam455@gmail.com>
2026-06-21 15:09:45 +0330

    style(protected-layout): rename bg-dashboard-bg class to bg-white-primary

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from '@/features/sidebar/components/sidebar';
 import { JSX } from 'react';
 export default function ProtectedLayout({ children } : { children: React.ReactNode }): JSX.Element {
     return (
-        <div className='p-4.5 bg-dashboard-bg h-screen overflow-hidden flex gap-4.5'>
+        <div className='p-4.5 bg-white-primary h-screen overflow-hidden flex gap-4.5'>
             <Sidebar />
            <div className='flex-1'>
                 {children} 
