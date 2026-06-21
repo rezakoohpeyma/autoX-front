15e1c199fd79dc7aed500e8ed01ef1bc790f3dd3
Milad Afzali <miladsteam455@gmail.com>
2026-06-21 15:14:12 +0330

    feat(sidebar): add sidebar-footer

--- src/features/sidebar/components/sidebar-footer.tsx
+++ src/features/sidebar/components/sidebar-footer.tsx
@@ -1,8 +1,8 @@
 import { JSX } from 'react';
 export default function SidebarFooter(): JSX.Element {
     return (
-        <div>
-            
+        <div className='h-22 w-full p-6 border-t border-sidebar-border bg-white-secondary flex justify-center items-center'>
+            <button className='w-full py-2 rounded-lg text-primary cursor-pointer border border-[#CACACA] transition-all duration-300 delay-75 hover:text-white hover:bg-[#CACACA]'>Inbox</button>
         </div>
     )
 }
--- src/features/sidebar/components/sidebar.tsx
+++ src/features/sidebar/components/sidebar.tsx
@@ -3,13 +3,15 @@ import { sidebarWidth } from '../config/sidebar-width';
 import SidebarHeader from './sidebar-header';
 import SidebarCategory from './sidebar-category';
 import { sidebar } from '../config/sidebar';
+import SidebarFooter from './sidebar-footer';
 export default function Sidebar(): JSX.Element {
     return (
-        <div className='bg-white border border-sidebar-border size-full rounded-2xl overflow-hidden' style={{width: sidebarWidth}}>
+        <div className='bg-white flex justify-center items-start flex-col border border-sidebar-border size-full rounded-2xl overflow-hidden' style={{width: sidebarWidth}}>
             <SidebarHeader />
-            <div className='overflow-y-scroll w-full'>
+            <div className='overflow-y-scroll w-full flex-1'>
                 {sidebar.map((category, i) => <SidebarCategory category={category} key={i}/>)}
             </div>
+            <SidebarFooter />
         </div>
     )
 }
