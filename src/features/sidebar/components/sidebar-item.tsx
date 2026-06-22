2864ea4dcf6c1b46231be2d658a9a2377dc3fee7
Milad Afzali <miladsteam455@gmail.com>
2026-06-22 23:44:37 +0330

    feat(sidebar): add dropdown to sidebar

--- src/features/sidebar/components/sidebar-item.tsx
+++ src/features/sidebar/components/sidebar-item.tsx
@@ -4,7 +4,6 @@ import * as ReactIcons from "react-icons/fa6";
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import NavLink from "@/components/ui/nav-link";
@@ -12,6 +11,8 @@ import SidebarItemChild from "./sidebar-item-child";
 import { SidebarItemType } from '../types/sidebar-types';
 import { JSX } from 'react';
 import { useSidebarStore } from "../store/use-sidebar-store";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 
 interface SidebarItemProp {
   item: SidebarItemType;
@@ -24,10 +25,8 @@ export default function SidebarItem({ item, onSelectedId, selectedId }: SidebarI
   const isSelected = selectedId === item.id;
   const hasChild = !!item.hasChild;
   
-  // انتخاب آیکون
   const IconComponent = item.icon ? ReactIcons[item.icon] : ReactIcons.FaQuestion;
 
-  // مشخص کردن آیکون فلش (Angle Up/Down)
   const renderArrowIcon = () => {
     if (isCollapsed || !hasChild) return null;
     return isSelected ? (
@@ -37,15 +36,14 @@ export default function SidebarItem({ item, onSelectedId, selectedId }: SidebarI
     );
   };
 
-  // محتوای اصلی آیتم سایدبار
   const SidebarContent = (
     <NavLink
       href={item.link}
       onClick={() => onSelectedId(item.id)}
-      className="flex justify-between items-center h-10 rounded-lg p-3 cursor-pointer text-center w-full"
+      className="w-full h-10 p-3 flex justify-between items-center rounded-lg cursor-pointer text-[#5C5E64] text-center transition-all duration-300 delay-75 hover:text-black"
     >
       <span className="flex items-center gap-2">
-        <IconComponent className="text-[20px] text-[#5C5E64]" />
+        <IconComponent className="text-[20px]" />
         {!isCollapsed && (
           <span className="font-medium text-sm tracking-tight">
             {item.name}
@@ -53,35 +51,11 @@ export default function SidebarItem({ item, onSelectedId, selectedId }: SidebarI
         )}
       </span>
 
-      {/* نمایش فلش فقط وقتی سایدبار باز است */}
       {!isCollapsed && renderArrowIcon()}
     </NavLink>
   );
 
-  return (
-    <TooltipProvider>
-      {/* 
-        اگر سایدبار باز باشد، Tooltip را دورِ محتوا نمی‌کشیم (فقط خود محتوا را رندر می‌کنیم).
-        اگر بسته باشد، از ساختار Tooltip استفاده می‌کنیم.
-      */}
-      {isCollapsed ? (
-        <Tooltip>
-          <TooltipTrigger asChild>
-            {SidebarContent}
-          </TooltipTrigger>
-          <TooltipContent side="right">
-            <p>{item.name}</p>
-          </TooltipContent>
-        </Tooltip>
-      ) : (
-        /* وقتی سایدبار باز است، فقط محتوا را رندر کن (بدون تولتیپ) */
-        <div className="w-full">
-          {SidebarContent}
-        </div>
-      )}
-
-      {/* بخش زیرمجموعه‌ها (Sub-items) */}
-      {isSelected && (
+  const SidebarDropDownContent = (
         <div className="py-3 w-3/5 ml-auto flex justify-center items-stretch gap-3">
           <div className="w-0.5 bg-sidebar-border" />
           <div className="flex justify-center items-center flex-col gap-1 w-full">
@@ -90,7 +64,44 @@ export default function SidebarItem({ item, onSelectedId, selectedId }: SidebarI
             ))}
           </div>
         </div>
+  );
+
+  return (
+    <DropdownMenu>
+      {isCollapsed ? (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger>
+              {SidebarContent}
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent side="right">
+            <p>{item.name}</p>
+          </TooltipContent>
+        </Tooltip>
+      ) : (
+        <div className="w-full">
+          {SidebarContent}
+        </div>
       )}
-    </TooltipProvider>
+      
+        {isCollapsed && hasChild
+          ? 
+            <DropdownMenuContent side="right" className="w-43 p-2 border border-sidebar-border rounded-[12px]">
+              <DropdownMenuGroup>
+                <DropdownMenuLabel>{item.name}</DropdownMenuLabel>
+                  {item.hasChild?.map((subItem, i, arr) => (
+                    <DropdownMenuItem className={cn(
+                        "hover:text-black focus:bg-transparent p-0",
+                        i !== arr.length ? 'mb-1' : ''
+                    )} key={i}>
+                      <SidebarItemChild item={subItem} />
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+          : isSelected && SidebarDropDownContent
+        }
+    </DropdownMenu>
   );
 }
