f26b6219180de4603f6263fc0574fae419599270
Milad Afzali <miladsteam455@gmail.com>
2026-06-22 13:31:40 +0330

    refactor(logo): remove company name in logo.tsx

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,10 +3,7 @@ import logo from '@/assets/images/logo.png'
 import { JSX } from 'react';
 export default function Logo(): JSX.Element {
     return (
-        <div className='flex items-center gap-2 text-primary font-bold text-lg'>
-            <Image src={logo} alt='AuthX'/>
-            AutoX
-        </div>
+        <Image src={logo} alt='AuthX'/>
     )
 }
 
