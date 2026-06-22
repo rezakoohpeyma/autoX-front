f356215d7fbb5d432b1d7c38b8ed078b88162d57
Milad Afzali <miladsteam455@gmail.com>
2026-06-22 13:09:04 +0330

    style(sidebar-footer): add rounded corners to sidebar footer

--- src/features/sidebar/components/sidebar-footer.tsx
+++ src/features/sidebar/components/sidebar-footer.tsx
@@ -1,7 +1,7 @@
 import { JSX } from 'react';
 export default function SidebarFooter(): JSX.Element {
     return (
-        <div className='h-22 w-full p-6 border-t border-sidebar-border bg-white-secondary flex justify-center items-center'>
+        <div className='h-22 w-full p-6 border-t rounded-bl-2xl rounded-br-2xl border-sidebar-border bg-white-secondary flex justify-center items-center'>
             <button className='w-full py-2 rounded-lg text-primary cursor-pointer border border-[#CACACA] transition-all duration-300 delay-75 hover:text-white hover:bg-[#CACACA]'>Inbox</button>
         </div>
     )
