ae05b9988c2f43a1caca1f44d7db487776f60f71
Milad Afzali <miladsteam455@gmail.com>
2026-06-26 21:31:43 +0330

    feat(dashboard-header): add and use header-conrol-item.tsx

---
+++ src/components/ui/header-control-item.tsx
@@ -0,0 +1,52 @@
+import { ReactNode } from "react"
+import Link from "next/link"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdown-menu"
+import { Menus } from "./header-controls"
+
+export default function HeaderControlItem({
+  trigger,
+  label,
+  items,
+}: Menus) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="cursor-pointer">
+        {trigger}
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        {label && <DropdownMenuLabel>{label}</DropdownMenuLabel>}
+
+        <DropdownMenuGroup>
+          {items.map((item, index) => {
+            if (item.separator) {
+              return <DropdownMenuSeparator key={index} />
+            }
+
+            return (
+              <DropdownMenuItem key={index} onClick={item.onClick}>
+                {item.href ? (
+                  <Link href={item.href} className="flex justify-center items-center gap-1 text-black/60">
+                    {item.icon}
+                    <span>{item.label}</span>
+                  </Link>
+                ) : (
+                  item.label
+                )}
+              </DropdownMenuItem>
+            )
+          })}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- src/components/ui/header-controls.tsx
+++ src/components/ui/header-controls.tsx
@@ -1,11 +1,49 @@
-import { JSX } from 'react';
-import { FaGear, FaUser } from 'react-icons/fa6';
+
+import { JSX, ReactNode } from 'react';
+import { FaGear, FaIdCard, FaRightFromBracket, FaUser } from 'react-icons/fa6';
+import HeaderControlItem from './header-control-item';
+
+export type MenuItem = {
+  label?: string
+  icon?: ReactNode,
+  href?: string
+  onClick?: () => void
+  separator?: boolean
+}
+
+export type Menus = {
+  trigger: ReactNode
+  label?: string
+  items: MenuItem[]
+}
+
+const menus: Menus[] = [
+    {
+        trigger: <FaUser />,
+        label: "User",
+        items: [
+            {
+                label: "Show Detail",
+                icon: <FaIdCard />,
+                href:'#'
+                
+            },
+            { separator: true },
+            {
+                label: "Log out",
+                icon: <FaRightFromBracket />,
+                href:'#',
+                
+            },
+        ]
+    }
+]
 export default function HeaderControls(): JSX.Element {
     return (
         <div className='flex justify-center items-center gap-3 text-[#A6ADBF] text-base'>
-            <button className='cursor-pointer'>
-                <FaUser />
-            </button>
+            {menus.map((menu, i) => 
+                <HeaderControlItem label={menu.label} trigger={menu.trigger}  items={menu.items} key={i}/>
+            )}
             <button className='cursor-pointer'>
                 <FaGear />
             </button>
