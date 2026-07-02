8cc855a01d9f352e6faab672eef5a5c177422bc4
Milad Afzali <miladsteam455@gmail.com>
2026-07-03 02:20:09 +0330

    feat(main-providers): add react-toasty & tanstack/react-query providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,28 @@
+'use client'
+import { ToastContainer } from 'react-toastify';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { ComponentProps, JSX } from 'react';
-
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 type ProvidersProps = ComponentProps<'div'>;
 
+const queryClient = new QueryClient();
 export default function Providers( { children } : ProvidersProps): JSX.Element {
     return (
-        <div>
+        <QueryClientProvider client={queryClient}>
             <TooltipProvider>
+                <ToastContainer 
+                    position='top-center' 
+                    pauseOnHover
+                    theme='light'
+                    autoClose={5000}
+                    draggable
+                />
                 {children}
             </TooltipProvider>
-        </div>
+        </QueryClientProvider>
     )
 }
 
