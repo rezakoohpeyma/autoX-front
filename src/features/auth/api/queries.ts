f5aa048672d7c872ff661ff80bd46ef023bd2eb5
Milad Afzali <miladsteam455@gmail.com>
2026-07-03 18:26:04 +0330

    fix(auth-api): add AuthType to getMe

--- src/features/auth/api/queries.ts
+++ src/features/auth/api/queries.ts
@@ -1,6 +1,7 @@
 import { cFetch } from "@/services/cFetch";
+import { AuthType } from "../schema";
 
 export async function getMe(){
     const data = await cFetch('/api/auth/me');
-    return data;
+    return data as AuthType;
 }
