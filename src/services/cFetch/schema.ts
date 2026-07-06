d2a761b7b1da2651487b8f996107bf8cc8b0e21d
Milad Afzali <miladsteam455@gmail.com>
2026-07-06 15:34:01 +0330

    refactor(cFetch): remove all of cFetch files

--- src/services/cFetch/schema.ts
+++
@@ -1,7 +0,0 @@
-import { authRoutes } from "@/features/auth/api/routes";
-import { createSchema } from "@better-fetch/fetch";
-
-export const schema = createSchema({
-    // Auth Feature
-    ...authRoutes,
-})
