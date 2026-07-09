7a76e7c7ac39a10a829e126298139f7f7ea90359
Milad Afzali <miladsteam455@gmail.com>
2026-07-09 15:24:24 +0330

    feat: Add some new routes
    - Add documents route
    - Add all of cartables routes
    - Add all of orders routes
    - Add products & productsUs routes
    - Add workflow builder route
    - Add all of user management routes
    - Add all of audit log routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,7 +1,38 @@
 export const routes = {
     home: '/',
     dashboard: '/dashboard',
+    documents: "/documents",
+
+    // Auth Routes
     signIn: '/sign-in',
     signUp: '/sign-up',
-    resetPassword: '/reset-password'
+    resetPassword: '/reset-password',
+
+    // Cartables Routes
+    cartables: "/cartables",
+    cartablesCustomerExpert: "/cartables/customer-expert",
+    cartablesShipping: "/cartables/shipping",
+    cartablesFinance: "/cartables/finance",
+
+    // Orders Routes
+    orders: "/orders",
+    ordersAll: "/orders/all",
+    ordersProcessTracking: "/orders/process-tracking",
+
+    products: "/products",
+    productsUs: "/products-us",
+    
+    workflowBuilder: "/workflow-builder",
+
+    // User Management Routes
+    userManagement: "/user-management",
+    userManagementUsers: "/user-management/users",
+    userManagementRoles: "/user-management/roles",
+    userManagementPermissions: "/user-management/permissions",
+
+    // Audit Log Routes
+    auditLog: "/audit-log",
+    auditLogUsers: "/audit-log/users",
+    auditLogOrders: "/audit-log/orders"
+
 }
