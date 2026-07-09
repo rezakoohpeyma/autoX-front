5eab2894ffd03fbdfa2cfefe3172eb24ce40d398
Milad Afzali <miladsteam455@gmail.com>
2026-07-09 20:48:53 +0330

    refactor(sidebar): add hover style to sidebar-item-child

--- src/features/sidebar/components/sidebar-item-child.tsx
+++ src/features/sidebar/components/sidebar-item-child.tsx
@@ -6,7 +6,7 @@ type SidebarItemChildProps = {
 } & ComponentProps<'a'>
 export default function SidebarItemChild({ item } : SidebarItemChildProps): JSX.Element {
     return (
-        <NavLink href={item.link} className='w-full h-8 p-2 text-xs text-[#5C5E64] rounded-lg capitalize'>
+        <NavLink href={item.link} className='w-full h-8 p-2 text-xs text-[#5C5E64] rounded-lg capitalize transition-all duration-300 delay-75 hover:bg-[#F6F6F6]'>
             {item.name}
         </NavLink>
     )
