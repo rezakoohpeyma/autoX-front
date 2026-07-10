b4113ecddfd3449d2514096558f24933a101b57d
Milad Afzali <miladsteam455@gmail.com>
2026-07-10 18:12:17 +0330

    feat(main-provider): add NuqsAdapter

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { ToastContainer } from 'react-toastify';
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { ComponentProps, JSX } from 'react';
 import {
@@ -12,16 +13,18 @@ const queryClient = new QueryClient();
 export default function Providers( { children } : ProvidersProps): JSX.Element {
     return (
         <QueryClientProvider client={queryClient}>
-            <TooltipProvider>
-                <ToastContainer 
-                    position='top-center' 
-                    pauseOnHover
-                    theme='light'
-                    autoClose={5000}
-                    draggable
-                />
-                {children}
-            </TooltipProvider>
+            <NuqsAdapter>
+                <TooltipProvider>
+                    <ToastContainer 
+                        position='top-center' 
+                        pauseOnHover
+                        theme='light'
+                        autoClose={5000}
+                        draggable
+                    />
+                    {children}
+                </TooltipProvider>
+            </NuqsAdapter>
         </QueryClientProvider>
     )
 }
