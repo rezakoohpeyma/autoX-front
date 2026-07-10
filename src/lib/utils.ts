a23b62f6b5d71d98f7269753d6e32a3a03e269a3
Milad Afzali <miladsteam455@gmail.com>
2026-07-10 17:58:44 +0330

    feat(utils): add formDate & formDateTime func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
+import { format } from "date-fns";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string | Date) {
+  return format(new Date(date), "yyyy/MM/dd");
+}
+
+export function formatDateTime(date: string | Date) {
+  return format(new Date(date), "yyyy/MM/dd HH:mm");
 }
