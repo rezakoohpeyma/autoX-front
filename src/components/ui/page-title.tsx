4b556c1121c2f0857c6fc5a0467a15bc00f91810
Milad Afzali <miladsteam455@gmail.com>
2026-07-12 22:53:12 +0330

    refactor(page-title): add some style

--- src/components/ui/page-title.tsx
+++ src/components/ui/page-title.tsx
@@ -10,12 +10,12 @@ export default function PageTitle({ title, description, className, ...other } : 
     return (
         <div className={
             cn(
-                'mt-2',
+                'mt-2 mb-8.5',
                  className || ''
             )
             } 
             {...other}>
-            <h2 className='text-2xl capitalize mb-2'>{title}</h2>
+            <h2 className='text-2xl capitalize mb-1'>{title}</h2>
             <p className='text-primary/60'>{description}</p>
         </div>
     )
