935d2b5ddfe628506d07e1acefa52c6b04384f2f
Milad Afzali <miladsteam455@gmail.com>
2026-07-12 22:54:34 +0330

    refactor: move passwordSchemas from auth/schema to global schemas

--- src/features/auth/schema.ts
+++ src/features/auth/schema.ts
@@ -1,17 +1,5 @@
-import { phoneNumberSchema, rolesSchema, permissionsSchema } from '@/schemas'
+import { phoneNumberSchema, rolesSchema, permissionsSchema, passwordSchema } from '@/schemas'
 import { z } from 'zod'
-
-// Reusable Schemas
-
-export const passwordSchema = z
-    .string()
-    .min(8, "Password must be at least 8 characters long")
-    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .regex(/[0-9]/, 'Password must contain at least one number')
-    .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
-    .refine(val => !val.includes(' '), 'Password must not contain spaces')
-
 // Form Schemas
 
 export const signInFormSchema = z.object({
