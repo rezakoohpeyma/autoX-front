725693abcb7619c98c65eb3926dbb91399f9e6f2
Milad Afzali <miladsteam455@gmail.com>
2026-07-15 00:50:23 +0330

    refactor(form): finish Multi-selected & add custom className to ErrorMessage

--- src/components/ui/form/components/error-message.tsx
+++ src/components/ui/form/components/error-message.tsx
@@ -1,11 +1,17 @@
+import { cn } from '@/lib/utils';
 import { ComponentProps, JSX } from 'react';
 import { HiOutlineExclamationTriangle } from 'react-icons/hi2';
 
 type ErrorMessageProps = ComponentProps<'p'>;
 
-export default function ErrorMessage({ children ,...other } : ErrorMessageProps): JSX.Element {
+export default function ErrorMessage({ children, className, ...other } : ErrorMessageProps): JSX.Element {
     return (
-        <div className="mt-2 ml-2 flex gap-1 text-sm text-red-500 font-semibold" {...other}>
+        <div className={
+            cn(
+                "mt-2 ml-2 flex gap-1 text-sm text-red-500! font-semibold",
+                className
+            )
+        } {...other}>
             <HiOutlineExclamationTriangle className="text-[1.2em]" />
             <p>{children}</p>
         </div>
--- src/components/ui/form/components/multi-select/index.tsx
+++ src/components/ui/form/components/multi-select/index.tsx
@@ -1,27 +1,37 @@
 'use client';
-import { ComponentProps, JSX, useId, useState } from 'react';
+import { ComponentProps, JSX, useId } from 'react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from '../../../dropdown-menu'; 
 import { useFormStyleContext } from '../../hooks/use-form-style-context';
-import { FieldValues, useFormContext } from 'react-hook-form';
+import { Controller, FieldValues, Path, useFormContext } from 'react-hook-form';
 import { cn } from '@/lib/utils';
-import { Button } from '../../../button';
 import { Checkbox } from '../../../checkbox';
 
-type  MultiSelectProps<TOptions> = {
-    defaultTxt?: string;
-    options: TOptions[]
-    getOptionValue: (options: TOptions) => string;
-    getOptionLabel: (options: TOptions) => string | number;
-} & ComponentProps<'div'>
+type MultiSelectProps<
+  TForm extends FieldValues,
+  TOption,
+  TValue extends string | number
+> = {
+  nameId: Path<TForm>;
 
-export default function MultiSelect<TForm extends FieldValues, TOptions>({ 
+  options: TOption[];
+
+  defaultTxt?: string;
+
+  getOptionValue(option: TOption): TValue;
+
+  getOptionLabel(option: TOption): React.ReactNode;
+} & ComponentProps<"div">;
+
+export default function MultiSelect<TForm extends FieldValues, TOptions, TValue extends string | number>({ 
+        nameId,
         children,
         defaultTxt = 'Select',
         options,
         getOptionValue,
-    } : MultiSelectProps<TOptions>
+        getOptionLabel,
+    } : MultiSelectProps<TForm, TOptions, TValue>
     ): JSX.Element {
-        
+
     const { 
         inputFieldClasses,
         inputBoxClasses,
@@ -30,12 +40,7 @@ export default function MultiSelect<TForm extends FieldValues, TOptions>({
     const {
         control
     } = useFormContext<TForm>()
-    const [SelectedValues, setSelectedValues] = useState<string[]>([]);
 
-
-    function handleCheckedChange(newValue: string){
-        setSelectedValues(lastValues => lastValues.includes(newValue) ? lastValues.filter(value => value !== newValue) : [...lastValues, newValue])
-    }
     const selectId = useId()
     return (
         <div className={
@@ -44,37 +49,68 @@ export default function MultiSelect<TForm extends FieldValues, TOptions>({
                 inputFieldClasses
             )
         }>
-            {children && <label id={selectId}>{children}</label>}
+            {children && <label className='block' htmlFor={selectId}>{children}</label>}
 
-            <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button 
-                    type='button' 
-                    id={selectId} 
-                    variant='outline'
-                    className={
-                        cn(
-                            '',
-                            inputBoxClasses
-                        )
-                    }
-                >
-                    {defaultTxt}
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuGroup>
-                    {options.map((option) => 
-                        <DropdownMenuItem key={getOptionValue(option)}>
-                            <Checkbox 
-                                checked={SelectedValues?.includes(getOptionValue(option))} 
-                                onCheckedChange={() => handleCheckedChange(getOptionValue(option))}
-                            />
-                        </DropdownMenuItem>
-                    )}
-                </DropdownMenuGroup>
-            </DropdownMenuContent>
-        </DropdownMenu>
+            <Controller 
+                control={control}
+                name={nameId}
+                render={({ field }) =>{ 
+                    const selectedValues = (field.value ?? []) as TValue[];
+
+                    const toggleValue = (value: TValue) => {
+                    const exists = selectedValues.includes(value);
+
+                    const newValues = exists
+                        ? selectedValues.filter(item => item !== value)
+                        : [...selectedValues, value];
+
+                    field.onChange(newValues);
+                    };
+                    
+                    return (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <input 
+                                    type='button' 
+                                    id={selectId} 
+                                    className={
+                                        cn(
+                                            'text-left px-4 cursor-pointer',
+                                            inputBoxClasses
+                                        )
+                                    }
+                                    defaultValue={defaultTxt}
+                                />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuGroup>
+                                    {options.map((option, i) => 
+                                        <DropdownMenuItem 
+                                            key={getOptionValue(option)} 
+                                            className='cursor-pointer!' 
+                                            onSelect={(e) => e.preventDefault()}
+                                            asChild
+                                        >
+                                            <label htmlFor={String(i)} className='cursor-pointer'>
+                                                <Checkbox
+                                                    id={String(i)}
+                                                    className={cn(
+                                                        "data-[state=checked]:bg-white data-[state=checked]:text-primary border-primary",
+                                                        'cursor-pointer')}
+                                                    checked={field.value?.includes(getOptionValue(option)) ?? false}
+                                                    onCheckedChange={() => toggleValue(getOptionValue(option))}
+                                                />
+                                                {getOptionLabel(option)}
+                                            </label>
+                                        </DropdownMenuItem>
+                                    )}
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    )
+                }
+            }
+            />
         </div>
     )
 }
